94e9a3397967bc8a7b00c8fd30d3655db0b4c2db
Roberti Yavier Amarante Garcia <yavier.ag@gmail.com>
2016-10-14 18:10:44 -0400

    Creacios de mis clases

---
+++ Entidades/Vendedores.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+   public class Vendedores
+    {
+        public int VendedorId { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Cedula { get; set; }
+
+        public string TelefonoFijo { get; set; }
+
+        public string TelefonoMovil { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string TipoVehiculo { get; set; }
+    }
+}
