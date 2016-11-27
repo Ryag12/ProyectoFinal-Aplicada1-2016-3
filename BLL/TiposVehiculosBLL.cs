dda585b9c62a9be576f3b2863e80a16592338f4e
Roberti Yavier Amarante Garcia <yavier.ag@gmail.com>
2016-11-27 18:20:47 -0400

    AntiRobo

---
+++ BLL/TiposVehiculosBLL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entidades;
+using DAL;
+namespace BLL
+{
+   public  class TiposVehiculosBLL
+    {
+        public static bool Insertar(TiposVehiculos tVehiculos)
+        {
+            bool resultado = false;
+            using (var conexion  =new  ProyectoFinalDb())
+            {
+                try
+                {
+                    conexion.TiposVehiculos.Add(tVehiculos);
+                    conexion.SaveChanges();
+                    resultado = true;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                return resultado;
+            }
+        }
+    }
+}
